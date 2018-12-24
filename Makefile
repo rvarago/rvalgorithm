bd18824fc9effd386ecd7c2c399fd38aee66fe12
rvarago <varago.rafael@gmail.com>
2018-12-24 17:00:31 +0100

    Create unit tests to ensure the expected behavior
    To execute unit tests, invoke the new make target: test

--- Makefile
+++ Makefile
@@ -1,14 +1,20 @@
-.PHONY: all mk gen build
+.PHONY: all mk gen compile test
 
 BUILD_DIR = build
 
-all: mk gen compile
+all: mk dep gen compile
+
+test: all
+	cd $(BUILD_DIR) && ctest . 
 
 compile: gen
 	cd $(BUILD_DIR) && cmake --build .
 
-gen: mk
+gen: dep
 	cd $(BUILD_DIR) && cmake ..
+
+dep: mk
+	cd $(BUILD_DIR) && conan install ..
 
 mk:
 	-mkdir -p $(BUILD_DIR)
---
+++ test/rvalgorithm_test.cpp
@@ -0,0 +1,30 @@
+#include <gtest/gtest.h>
+
+extern "C" {
+#include "rvalgorithm/rvalgorithm.h"
+}
+
+struct person_t
+{
+	char name[10];
+	int id;
+};
+
+int
+id_equals_20(const void *el)
+{
+	return ((const struct person_t*) el)->id == 20;
+}
+
+TEST(rvalgorithm, find)
+{
+	const struct person_t arr[] = {{"P1", 10}, {"P2", 20}, {"P3", 30}, {"P4", 40}};
+	const void *arr_f = find(arr, sizeof(struct person_t), 4, id_equals_20);
+	EXPECT_STREQ("20", ((struct person_t*) arr_f)->name);
+}
+
+int main(int argc, char **argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
