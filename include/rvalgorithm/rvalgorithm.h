6ba3711c667362801a84ada8eed080c6c9d8522a
rvarago <varago.rafael@gmail.com>
2018-12-25 17:06:36 +0100

    Define consistent format across the project

--- include/rvalgorithm/rvalgorithm.h
+++ include/rvalgorithm/rvalgorithm.h
@@ -3,20 +3,20 @@
 
 #include <stdlib.h>
 
-typedef void( *processor_t )( const void *element );
+typedef void(*processor_t)(const void *element);
 
-typedef int( *predicate_t )( const void *element );
+typedef int(*predicate_t)(const void *element);
 
 /*
 * @brief apply processor to each element of collection.
 */
 void
-foreach( const void *collection, size_t size, size_t nelems, processor_t processor );
+foreach(const void *collection, size_t size, size_t nelems, processor_t processor);
 
 /*
 * @brief find and return the first element from collection which satisfies pred.
 */
 const void*
-find( const void *collection, size_t size, size_t nelems, predicate_t pred );
+find(const void *collection, size_t size, size_t nelems, predicate_t pred);
 
 #endif
--- src/rvalgorithm.c
+++ src/rvalgorithm.c
@@ -1,28 +1,26 @@
 #include "rvalgorithm/rvalgorithm.h"
 
 void
-foreach( const void *collection, size_t size, size_t nelems, processor_t processor )
+foreach(const void *collection, size_t size, size_t nelems, processor_t processor) 
 {
-	const char *base = ( const char * ) collection;
+	const char *base = (const char *) collection;
 	int i;
 
-	for( i = 0; i < nelems; ++i )
-	{
-		processor( base );
+	for(i = 0; i < nelems; ++i) {
+		processor(base);
 		base += size;
 	}
 }
 
 const void*
-find( const void *collection, size_t size, size_t nelems, predicate_t pred )
+find(const void *collection, size_t size, size_t nelems, predicate_t pred)
 {
-	const char *base = ( const char * ) collection;
+	const char *base = (const char *) collection;
 	int i;
 
-	for( i = 0; i < nelems && pred( base ) == 0; ++i )
-	{
+	for(i = 0; i < nelems && pred(base) == 0; ++i) {
 		base += size;
 	}
 	
-	return i < nelems ? base : NULL ;
+	return i < nelems ? base : NULL;
 }
