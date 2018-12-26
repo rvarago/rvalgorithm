6654762da84f9c9bcf1cd2de1338f77ba0dc2bd3
rvarago <varago.rafael@gmail.com>
2018-12-26 13:31:49 +0100

    Separate the tests for rv_find_if_n

--- test/find_test.cpp
+++ test/find_test.cpp
@@ -22,12 +22,16 @@ id_equals_50(const void *person)
 	return ((const struct person_t*) person)->id == 50;
 }
 
-TEST(rvalgorithm, find_if_n)
+TEST(findIfN, containsThePerson)
 {
-
 	const struct person_t person_coll[] = {{"P1", 10}, {"P2", 20}, {"P3", 30}, {"P4", 40}};
 	const void *person_p2_found = rv_find_if_n(person_coll, sizeof(struct person_t), 4, id_equals_20);
 	EXPECT_STREQ("P2", ((struct person_t*) person_p2_found)->name);
+}
+
+TEST(findIfN, doesntContainThePerson)
+{
+	const struct person_t person_coll[] = {{"P1", 10}, {"P2", 20}, {"P3", 30}, {"P4", 40}};
 	const void *person_p5_not_found = rv_find_if_n(person_coll, sizeof(struct person_t), 4, id_equals_50);
 	EXPECT_EQ(rv_end(person_coll, sizeof(struct person_t), 4), person_p5_not_found);
 }
