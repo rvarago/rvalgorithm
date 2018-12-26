06ce84c871cacf4e37291e5ce8dc717ac2143977
rvarago <varago.rafael@gmail.com>
2018-12-26 14:10:04 +0100

    Refactor to use const pointer to const instead of only to const

--- src/end.c
+++ src/end.c
@@ -2,6 +2,5 @@
 
 const void*
 rv_end(const void *begin, size_t size, size_t n) {
-	const char *begin_as_char_ptr = (const char *) begin;
-	return begin_as_char_ptr + size * n;
+	return ((const char *) begin) + size * n;
 }
--- src/find.c
+++ src/find.c
@@ -5,6 +5,6 @@ const void*
 rv_find_if_n(const void *collection, size_t size, size_t n, predicate_t pred)
 {
 	const char *begin = (const char *) collection;
-	for(const void *end = rv_end(collection, size, n); begin != end && !pred(begin); begin += size) {}
+	for(const void * const end = rv_end(collection, size, n); begin != end && !pred(begin); begin += size) {}
 	return begin;
 }
